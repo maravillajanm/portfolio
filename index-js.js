f7b8f3034863b721e67eb5e6a28c1025201eab11
marvjan <marvjan@users.noreply.github.com>
2021-03-07 21:27:02 +0800

    Adding GSAP Javascript Animation

--- index-js.js
+++ index-js.js
@@ -1,3 +1,5 @@
 function myFunction() {
     document.getElementById("myName").innerHTML = "Jan Michael Maravilla";
   }
+gsap.from(".hero", {duration: 1.5, y: "-100%"});
+gsap.from(".section", {delay: 1.5, duration: 1.5, y: "-100%", autoAlpha: 0});
